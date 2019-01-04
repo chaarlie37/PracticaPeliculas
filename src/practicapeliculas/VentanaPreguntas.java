efe434867691101e401d4e056653bee59bb18863
chaarlie37 <carlos_sm_37@yahoo.es>
2019-01-04 14:04:05 +0100

    prueba2

--- src/practicapeliculas/VentanaPreguntas.java
+++ src/practicapeliculas/VentanaPreguntas.java
@@ -139,7 +139,9 @@ public class VentanaPreguntas extends javax.swing.JFrame {
             ofrecerNuevaPregunta();
         }        
         else if(contadorPistas > 4 && contadorPreguntas == 6){
-            finalizar();            
+            finalizar();      
+            // prueba blablabla
+            
         }
         
     }//GEN-LAST:event_bResponderActionPerformed
